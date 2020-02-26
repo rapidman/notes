f21b63dcfbc3d16388254b47934de1ea5d4c0ef4
Timur Saidov <rapidman.stv@gmail.com>
2020-02-26 13:40:01 +0700

    Delete test.sql

--- test.sql
+++
@@ -1,4 +0,0 @@
-select * from audt.customer_location where event_timestamp is not null
-	and event_timestamp >= '2016-07-01 03:30:00'
-	and event_timestamp <= '2016-07-01 03:40:00'
-        and city_id in ('44')
